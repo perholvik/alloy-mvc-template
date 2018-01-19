032ff0113a326126488e6ffcc1f4d649094228a7
Malliusken <marianne.angel.olsen@knowit.no>
2018-01-19 12:23:32 +0100

    Add Episerver Find to solution

--- src/Alloy.Mvc.Template/Controllers/SearchPageController.cs
+++ src/Alloy.Mvc.Template/Controllers/SearchPageController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +8,8 @@ using EPiServer.Search;
 using AlloyTemplates.Business;
 using AlloyTemplates.Models.Pages;
 using AlloyTemplates.Models.ViewModels;
+using EPiServer.Find;
+using EPiServer.Find.Framework;
 using EPiServer.Web;
 using EPiServer.Web.Hosting;
 using EPiServer.Web.Mvc.Html;
@@ -17,90 +19,27 @@ namespace AlloyTemplates.Controllers
 {
     public class SearchPageController : PageControllerBase<SearchPage>
     {
-        private const int MaxResults = 40;
-        private readonly SearchService _searchService;
-        private readonly ContentSearchHandler _contentSearchHandler;
-        private readonly UrlResolver _urlResolver;
-        private readonly TemplateResolver _templateResolver;
 
-        public SearchPageController(
-            SearchService searchService, 
-            ContentSearchHandler contentSearchHandler, 
-            TemplateResolver templateResolver,
-            UrlResolver urlResolver)
+
+        public SearchPageController()
         {
-            _searchService = searchService;
-            _contentSearchHandler = contentSearchHandler;
-            _templateResolver = templateResolver;
-            _urlResolver = urlResolver;
+
         }
 
         [ValidateInput(false)]
-        public ViewResult Index(SearchPage currentPage, string q)
+        public ViewResult Index(SearchPage currentPage, string query)
         {
-            var model = new SearchContentModel(currentPage)
-                {
-                    SearchServiceDisabled = !_searchService.IsActive,
-                    SearchedQuery = q
-                };
-
-            if(!string.IsNullOrWhiteSpace(q) && _searchService.IsActive)
+            var model = new SearchContentModel(currentPage, query);
+            if (string.IsNullOrEmpty(query))
             {
-                var hits = Search(q.Trim(),
-                    new[] { SiteDefinition.Current.StartPage, SiteDefinition.Current.GlobalAssetsRoot, SiteDefinition.Current.SiteAssetsRoot }, 
-                    ControllerContext.HttpContext, 
-                    currentPage.Language?.Name).ToList();
-                model.Hits = hits;
-                model.NumberOfHits = hits.Count();
+                return View(model);
             }
 
+            var unifiedSearch = SearchClient.Instance.UnifiedSearchFor(query);
+            model.Results = unifiedSearch.GetResult();
             return View(model);
         }
 
-        /// <summary>
-        /// Performs a search for pages and media and maps each result to the view model class SearchHit.
-        /// </summary>
-        /// <remarks>
-        /// The search functionality is handled by the injected SearchService in order to keep the controller simple.
-        /// Uses EPiServer Search. For more advanced search functionality such as keyword highlighting,
-        /// facets and search statistics consider using EPiServer Find.
-        /// </remarks>
-        private IEnumerable<SearchContentModel.SearchHit> Search(string searchText, IEnumerable<ContentReference> searchRoots, HttpContextBase context, string languageBranch)
-        {
-            var searchResults = _searchService.Search(searchText, searchRoots, context, languageBranch, MaxResults);
 
-            return searchResults.IndexResponseItems.SelectMany(CreateHitModel);
-        }
-
-        private IEnumerable<SearchContentModel.SearchHit> CreateHitModel(IndexResponseItem responseItem)
-        {
-            var content = _contentSearchHandler.GetContent<IContent>(responseItem);
-            if (content != null && HasTemplate(content) && IsPublished(content as IVersionable))
-            {
-                yield return CreatePageHit(content);
-            }
-        }
-
-        private bool HasTemplate(IContent content)
-        {
-            return _templateResolver.HasTemplate(content, TemplateTypeCategories.Page);
-        }
-
-        private bool IsPublished(IVersionable content)
-        {
-            if (content == null)
-                return true;
-            return content.Status.HasFlag(VersionStatus.Published);
-        }
-
-        private SearchContentModel.SearchHit CreatePageHit(IContent content)
-        {
-            return new SearchContentModel.SearchHit
-                {
-                    Title = content.Name,
-                    Url = _urlResolver.GetUrl(content.ContentLink),
-                    Excerpt = content is SitePageData ? ((SitePageData) content).TeaserText : string.Empty
-                };
-        }
     }
 }
--- src/Alloy.Mvc.Template/Models/ViewModels/SearchContentModel.cs
+++ src/Alloy.Mvc.Template/Models/ViewModels/SearchContentModel.cs
@@ -1,24 +1,18 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using AlloyTemplates.Models.Pages;
+using EPiServer.Find.UnifiedSearch;
 
 namespace AlloyTemplates.Models.ViewModels
 {
     public class SearchContentModel : PageViewModel<SearchPage>
     {
-        public SearchContentModel(SearchPage currentPage) : base(currentPage)
+        public SearchContentModel(SearchPage currentPage, string query) : base(currentPage)
         {
+            Query = query;
         }
 
-        public bool SearchServiceDisabled { get; set; }
-        public string SearchedQuery { get; set; }
-        public int NumberOfHits { get; set; }
-        public IEnumerable<SearchHit> Hits { get; set; }
+        public string Query { get; private set; }
+        public UnifiedSearchResults Results { get; set; }
 
-        public class SearchHit
-        {
-            public string Title { get; set; }
-            public string Url { get; set; }
-            public string Excerpt { get; set; }
-        }
     }
 }
